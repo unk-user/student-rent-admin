fc8d72b0e557e347331ca6d90cba3c60ad255f00
Hamza <144457566+unk-user@users.noreply.github.com>
2024-05-18 12:41:29 +0100

    update routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,41 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Signup from './pages/signupPage/Signup';
 import Login from './pages/loginPage/Login';
-import { loginAction, logout, signupAction } from './utils/authActions';
-import RootComponent from './components/RootComponent';
-import ClientRoute from './student/components/ClientLayout';
-import AuthContext from './context/AuthProvider';
-import { useContext } from 'react';
+import Signup from './pages/signupPage/Signup';
 import axios from 'axios';
-import HomePage from './student/pages/home/HomePage';
-import ListingPage from './student/pages/listing/ListingPage';
-import LandlordLayout from './landlord/components/LandlordLayout';
-import PropertiesPage from './landlord/pages/property-details/PropertiesPage';
-import PropertyDetailsPage from './landlord/pages/property-details/PropertyDetailsPage';
-import EditProperty from './landlord/pages/property-edit/EditProperty';
+import LandlordLayout from './components/LandlordLayout';
+import PropertiesPage from './pages/property-details/PropertiesPage';
+import PropertyDetailsPage from './pages/property-details/PropertyDetailsPage';
+import EditProperty from './pages/property-edit/EditProperty';
 
 function App() {
-  const authContext = useContext(AuthContext);
   axios.defaults.withCredentials = true;
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <RootComponent />,
-    },
-    {
       path: '/logout',
-      loader: logout,
       element: <div>logout</div>,
     },
     {
       path: '/signup',
-      action: signupAction({ authContext }),
       element: <Signup />,
     },
     {
       path: '/login',
-      action: loginAction({ authContext }),
       element: <Login />,
     },
     {
-      path: '/client',
-      element: <ClientRoute />,
-      children: [
-        {
-          path: '',
-          element: <HomePage />,
-        },
-        {
-          path: ':listingId',
-          element: <ListingPage />,
-        },
-      ],
-    },
-    {
-      path: '/landlord',
+      path: '/dashboard',
       element: <LandlordLayout />,
       children: [
         {
-          path: 'properties',
-          element: <PropertiesPage />,
-          children: [{ path: ':propertyId', element: <PropertyDetailsPage /> }],
+          path: '',
+          element: <div>Nothing to overview for now</div>,
         },
-        { path: 'edit', element: <EditProperty /> },
-        { path: 'edit/:propertyId', element: <EditProperty /> },
+        {
+          path: 'properties',
+          element: <PropertiesPage />
+        },
+        { path: 'properties/edit', element: <EditProperty /> },
+        { path: 'properties/edit/:propertyId', element: <EditProperty /> },
       ],
     },
   ]);
