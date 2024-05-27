da87d8e987d2f0afd8e193b16da0eb63ec671fe2
Hamza <144457566+unk-user@users.noreply.github.com>
2024-05-27 14:21:35 +0100

    add loading state

--- src/components/LandlordLayout.jsx
+++ src/components/LandlordLayout.jsx
@@ -7,9 +7,9 @@ import useQueryAuth from '@/hooks/useQueryAuth';
 
 function LandlordLayout() {
   const isMobile = useMediaQuery('(max-width: 767px');
-  useQueryAuth({queryKey: ['auth'], url: '/me'});
+  const { status } = useQueryAuth({ queryKey: ['auth'], url: '/me' });
 
-  return (
+  return status === 'success' ? (
     <article className="h-max relative flex md:ml-[76px]">
       {isMobile ? <MobileNavbar /> : <Navbar />}
       <div className="flex-1 flex flex-col rounded-r-xl max-md:rounded-t-xl min-h-screen max-md:min-h-[calc(100vh-64px)] max-md:mb-[64px]">
@@ -17,6 +17,10 @@ function LandlordLayout() {
         <Outlet />
       </div>
     </article>
+  ) : (
+    status === 'pending' && (
+      <span className="absolute top-1/2 left-1/2 translate-y-1/2 -translate-x-1/2 loading loading-dots loading-lg"></span>
+    )
   );
 }
 
