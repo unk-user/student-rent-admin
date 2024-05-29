3faea6c3dd0ae95002985b1e6722b474f53f1b5e
Hamza <144457566+unk-user@users.noreply.github.com>
2024-05-29 23:44:02 +0100

    fix bug add bottom pagination

--- src/components/LandlordLayout.jsx
+++ src/components/LandlordLayout.jsx
@@ -12,7 +12,7 @@ function LandlordLayout() {
   return status === 'success' ? (
     <article className="h-max relative flex md:ml-[76px]">
       {isMobile ? <MobileNavbar /> : <Navbar />}
-      <div className="flex-1 flex flex-col rounded-r-xl max-md:rounded-t-xl min-h-screen max-md:min-h-[calc(100vh-64px)] max-md:mb-[64px]">
+      <div className="flex-1 flex flex-col min-h-screen max-md:min-h-[calc(100vh-64px)] max-md:mb-[64px]">
         <Header />
         <Outlet />
       </div>
